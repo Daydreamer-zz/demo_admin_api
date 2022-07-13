486085ba4ed8d5fc248a96e3ad864e08d496d925
Daydreamer_Z <shz1997@hotmail.com>
2022-07-13 18:05:15 +0800

    book demo api

--- oAuth/serializers.py
+++ oAuth/serializers.py
@@ -17,7 +17,9 @@ class NewUserSerializer(serializers.ModelSerializer):
 
 class BooksSerializer(serializers.ModelSerializer):
     id = serializers.PrimaryKeyRelatedField(read_only=True)
+    created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
+    updated_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
 
     class Meta:
         model = Books
-        fields = ["id", "name", "author"]
+        fields = ["id", "name", "author", "created_at", "updated_at"]
